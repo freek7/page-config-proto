183b7163c66a298a5b402a51ec62ffa77a249d98
Oleh Diian <oleh.diian@gmail.com>
2021-10-15 09:25:00 +0300

    prepare npm package

--- src/plugins/upc-page-builder.js
+++ src/plugins/upc-page-builder.js
@@ -1,4 +1,4 @@
-import UpcPanelBar from '@/components/upc-panel-bar.vue';
+// import UpcPanelBar from '@/components/upc-panel-bar.vue';
 import UpcPaymentPage from '@/components/upc-payment-page.vue';
 // import VueTheMask from 'vue-the-mask';
 
@@ -6,7 +6,7 @@ export default {
 	install(Vue) {
 		require('@/assets/styles.scss');
 		// Vue.use(VueTheMask);
-		Vue.component('upc-panel-bar', UpcPanelBar);
+		// Vue.component('upc-panel-bar', UpcPanelBar);
 		Vue.component('upc-payment-page', UpcPaymentPage);
 	}
 };
